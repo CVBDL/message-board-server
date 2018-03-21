112e9c1dca8e91ba10c1a5d7a2c6db2d7a6d31cc
zhongsp <patrick.zhongsp@gmail.com>
2018-03-21 09:58:21 +0800

    add test

---
+++ src/api/tweet/model.spec.ts
@@ -0,0 +1,24 @@
+import test from 'ava';
+
+import Tweet from "./model";
+
+
+test('should define model named "tweet"', t => {
+  const expectedModelName = 'tweet';
+
+  t.is(expectedModelName, Tweet.modelName);
+});
+
+test('should define mongoose model (1)', t => {
+  try {
+    new Tweet();
+    t.pass();
+
+  } catch(e) {
+    t.fail();
+  }
+});
+
+test('should define mongoose model (2)', t => {
+  t.is('function', typeof Tweet.find);
+});
